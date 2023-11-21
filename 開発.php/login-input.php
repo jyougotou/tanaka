d974fe6960033911b7c2e5c53f6a278f32933f39
YoshikaiTaiki <2201212@s.asojuku.ac.jp>
2023-11-21 12:19:28 +0900

    asdf

--- 開発.php/login-input.php
+++ 開発.php/login-input.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login-input.css">
+    <link rel="stylesheet" href="css/login-input.css">
     <style>
         /* Add this style to increase the size of the text boxes */
         input[type="text"], input[type="password"] {
