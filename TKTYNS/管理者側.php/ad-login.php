77b4ccff6e81e3dd7da98d7932f52b812a6c34eb
jyougotou <2201226@s.asojuku.ac.jp>
2023-11-28 10:33:05 +0900

    田中

--- TKTYNS/管理者側.php/ad-login.php
+++ TKTYNS/管理者側.php/ad-login.php
@@ -1,6 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../ad(css)/ad-login.css">
+    <title>ASOスポーツ用品サイト(管理者側)</title>
+</head>
+<body>
 管理者ログイン画面
 <form action="ad-list.php" method="post">
 管理者ID<input type="text" name="login"><br>
 パスワード<input type="password" name="password"><br>
 <input type="submit" value="ログイン">
 </form>
+</body>
+</html>
