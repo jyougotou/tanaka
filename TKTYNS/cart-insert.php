21fc5fba3991ce0831a30315c3ade8fccef7de6b
2201237 <2201237@s.asojuku.ac.jp>
2023-11-28 14:05:26 +0900

    変更

--- TKTYNS/cart-insert.php
+++ TKTYNS/cart-insert.php
@@ -1,7 +1,5 @@
 <?php session_start(); ?>
 <?php require 'header.php'; ?>
-<<<<<<< HEAD
-<<<<<<< HEAD
 <?php require 'db-connect.php'; ?>
 <?php
     if(!empty($_POST['shohin_number']) and !empty($_SESSION['Member']['member_number'])){
@@ -21,29 +19,5 @@
 
     require 'cart.php';
 
-=======
-=======
->>>>>>> d6631cb854ab352f60bca56715af9e0198929d5d
-<?php
-$id = $_POST['shohin_number'];
-if(!isset($_SESSION['Detail'])){
-    $_SESSION['Detail'] = [];
-}
-$count = 0;
-if(isset($_SESSION['Detail'][$id])){
-    $count = $_SESSION['Detail'][$id]['count'];
-}
-$_SESSION['Detail'][$id] = [
-    'shohin_number' => $_POST['shohin_number'],
-    'shohin_price' => $_POST['shohin_price'],
-    'count' => $count + $_POST['count']
-];
-echo '<p>カートに商品を追加しました。</p>';
-echo '<hr>';
-require 'cart.php';
-<<<<<<< HEAD
->>>>>>> d6631cb854ab352f60bca56715af9e0198929d5d
-=======
->>>>>>> d6631cb854ab352f60bca56715af9e0198929d5d
 ?>
 <?php require 'footer.php'; ?>
