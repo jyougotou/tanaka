250433c6d7e7e057d213b4d82ce48791b7ced057
YoshikaiTaiki <2201212@s.asojuku.ac.jp>
2023-11-28 14:54:54 +0900

    aa

---
+++ TKTYNS.php/customer-update-input.php
@@ -0,0 +1,49 @@
+<?php session_start(); ?>
+<?php require 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/customer-update-input.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$member_mei=$member_stay=$member_fon=$member_pass='';
+
+if(isset($_SESSION['Member'])){
+    $member_mei=$_SESSION['Member']['member_mei'];
+    $member_stay=$_SESSION['Member']['member_stay'];
+    $member_fon=$_SESSION['Member']['member_fon'];
+    $member_pass=$_SESSION['Member']['member_pass'];
+}
+//商品画面に遷移する
+echo '<h1>情報更新会員登録</h1>';
+echo '<form action = "product.php" method = "post">';
+echo '<input type = "submit" class = "back-btn" value = "戻る">';
+echo '</form>';
+//会員情報更新画面に遷移する
+echo '<form action = "customer-update-output.php" method = "post">';
+echo '<table>';
+//requiredで未入力項目の摘出,ユーザー情報の更新
+echo '<tr><td class = "form-label">ユーザー名</td><td>';
+echo '<input type="text" required name="member_mei" class = "form-input" value="', $member_mei, '">';
+echo '</td></tr>';
+echo '<tr><td class = "form-label">住所</td><td>';
+echo '<input type="text" required name="member_stay" class = "form-input" value="', $member_stay, '">';
+echo '</td></tr>';
+echo '<tr><td class = "form-label">電話番号</td><td>';
+echo '<input type="text" required name="member_fon" class = "form-input" value="', $member_fon, '">';
+echo '</td></tr>';
+echo '<tr><td class = "form-label">パスワード</td><td>';
+echo '<input type="password" required name="member_pass" class = "form-input" value="', $member_pass, '">';
+echo '</td></tr>';
+echo '</table>';
+echo '<input type = "submit" class = "submit-btn" value = "更新">';
+echo '</form>';
+?>
+</body>
+</html>
+
+<?php require 'footer.php'; ?>
