3ee33859dca4bdbf8f2dd8b80be95d0666be55f8
SRtamas <2201222@s.asojuku.ac.jp>
2023-11-30 12:51:43 +0900

    regist

--- TKTYNS/AD/ad-registration.php
+++ TKTYNS/AD/ad-registration.php
@@ -5,18 +5,18 @@
     <title>ad-registration</title>
 </head>
 <body>
-    <form action = "ad-Redistration Compulete.php" method = "post">
+    <form action = "ad-Registration Completed.php" method = "post">
     <table>
-        <tr><th>商品名</th><th>商品説明</th><th>在庫</th><th>値段</th><th>ブランド名</th><th>カテゴリー</th><th>画像</th></tr>
+        <tr><th>商品名</th><th>商品説明</th><th>値段</th><th>画像</th><th>ブランド名</th><th>カテゴリー</th><th>在庫</th></tr>
     </tr>
     <tr>
         <td><input type = "text" name = "shohin_mei"></td>
         <td><input type = "text" name = "shohin_setu"></td>
-        <td><input type = "text" name = "stock_kazu"></td>
         <td><input type = "text" name = "shohin_price"></td>
+        <td><input type = "text" name = "shohin_gazo"></td>
         <td><input type = "text" name = "shohin_burand"></td>
         <td><input type = "text" name = "shohin_kate"></td>
-        <td><input type = "text" name = "shohin_gazo"></td>
+        <td><input type = "text" name = "stock_kazu"></td>
     </tr>
     </table>
             <button type = "submit">登録</button>
