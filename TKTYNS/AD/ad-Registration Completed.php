7d66f6be1f3c302339f63f0b757afabd055af9fb
SRtamas <2201222@s.asojuku.ac.jp>
2023-12-01 11:33:15 +0900

    ad-regist 完成

--- TKTYNS/AD/ad-Registration Completed.php
+++ TKTYNS/AD/ad-Registration Completed.php
@@ -14,69 +14,43 @@
 </head>
 <body>
     <?php
-        $pdo = new PDO($connect, USER, PASS);
-        $sql = $pdo->prepare('insert into Shohin(shohin_mei, shohin_setu, shohin_price, shohin_gazo) values (?, ?, ?, ?)');
-        
+                    $pdo = new PDO($connect, USER, PASS);
+                    $sql = $pdo->prepare('insert into Shohin(shohin_mei, shohin_setu, shohin_price, shohin_gazo) values (?, ?, ?, ?)');
+                    $sql->execute([$_POST['shohin_mei'], $_POST['shohin_setu'], $_POST['shohin_price'], $_POST['shohin_gazo']]);
+                    $last = $pdo->lastInsertId();
+                    $detailSql = $pdo->prepare('insert into Detail(shohin_number,shohin_sport,shohin_burnd,shohin_kate) values (?,?,?,?)');
+                    $detailSql->execute([(int)$last, $_POST['shohin_sport'], $_POST['shohin_burnd'], $_POST['shohin_kate']]);
+                    $stockSql = $pdo->prepare('insert into Stock(shohin_number,konyu_kazu,stock_kazu) values (?,?,?)');
+                    $stockSql->execute([(int)$last,$_POST['konyu_kazu'],$_POST['stock_kazu']]);
+
         if(empty($_POST['shohin_mei'])){
             echo '商品名を入力してください。';
         } else if(empty($_POST['shohin_setu'])){
-            echo '商品説明を入力してください';
+            echo '商品説明を入力してください。';
         } else if(!preg_match('/^[0-9]+$/',$_POST['shohin_price'])){
             echo '商品価格を整数で入力してください。';
         } else if(empty($_POST['shohin_gazo'])) {
             echo '商品画像を選択してください。';
+        } else if(empty($_POST['shohin_sport'])) {
+            echo 'スポーツ名を入力してください。';
         } else if(empty($_POST['shohin_burnd'])){
-            echo 'ブランド名を入力してください';
+            echo 'ブランド名を入力してください。';
         } else if(empty($_POST['shohin_kate'])) {
             echo 'カテゴリーを入力してください。';
+        } else if(!preg_match('/^[0-9]+$/',$_POST['konyu_kazu'])) {
+            echo '購入数を入力してください。';
         } else if(!preg_match('/^[0-9]+$/',$_POST['stock_kazu'])) {
             echo '在庫数を入力してください。';
         } else {
-            // ここでDetailテーブルにデータを挿入するSQLを準備
-            $detailSql = $pdo->prepare('insert into Detail(shohin_burnd, shohin_kate) values (?, ?)');
-            $detailSql->execute([$_POST['shohin_burnd'], $_POST['shohin_kate']]);
-
-            // ここでStockテーブルにデータを挿入するSQLを準備
-            $stockSql = $pdo->prepare('insert into Stock(stock_kazu) values (?)');
-            $stockSql->execute([$_POST['stock_kazu']]);
-
-            // 商品情報をShohinテーブルに挿入
-            $sql->execute([$_POST['shohin_mei'], $_POST['shohin_setu'], $_POST['shohin_price'], $_POST['shohin_gazo']]);
             
-            echo '<font color = "red">追加に成功しました。</font>';
+            echo '<h1>登録が完了しました。</h1>';
         }
     ?>
-    <br><hr><br>
-    <table border="1">
-        <tr>
-            <th>商品名</th>
-            <th>商品説明</th>
-            <th>値段</th>
-            <th>画像</th>
-            <th>ブランド名</th>
-            <th>カテゴリー</th>
-            <th>在庫</th>
-        </tr>
-    <?php
-        // Shohin, Detail, Stock テーブルを結合してデータを取得
-        $result = $pdo->query('select * from Shohin, Detail, Stock');
-        
-        foreach ($result as $row){
-            echo '<tr>';
-            echo '<td>', $row['shohin_mei'], '</td>';
-            echo '<td>', $row['shohin_setu'], '</td>';
-            echo '<td>', $row['shohin_price'], '</td>';
-            echo '<td>', $row['shohin_gazo'], '</td>';
-            echo '<td>', $row['shohin_burnd'], '</td>';
-            echo '<td>', $row['shohin_kate'], '</td>';
-            echo '<td>', $row['stock_kazu'], '</td>';
-            echo '</tr>';
-            echo "\n";        
-        }
-    ?>
-    </table>
+    <form action="ad-registration.php" method="post">
+        <button type="submit">商品登録へ</button>
+    </form>
     <form action="ad-list.php" method="post">
-        <button type="submit">戻る</button>
+        <button type="submit">商品一覧へ</button>
     </form>
 </body>
 </html>
