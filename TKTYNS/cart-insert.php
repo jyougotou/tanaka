34dd80835f3f187a68d1c1afc2f8476bb91a1642
tazunokiyuto <148731141+tazunokiyuto@users.noreply.github.com>
2023-12-01 12:39:39 +0900

    cart

--- TKTYNS/cart-insert.php
+++ TKTYNS/cart-insert.php
@@ -1,6 +1,15 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/cart-insert.css">
+    <title>Document</title>
+</head>
+<body>
 <?php
     echo '<h1>購入情報</h1>';
     if(!empty($_POST['shohin_number']) and !empty($_SESSION['Member']['member_number'])){
@@ -14,8 +23,11 @@
             $sql=$pdo->prepare('update Cart set cart_kazu=cart_kazu+1 where shohin_number=? and member_number=?');
             $sql->execute([$_POST['shohin_number'],$_SESSION['Member']['member_number']]);
         }
-        echo '数量を増やしました';
+        echo '<p>数量を増やしました</p>';
     }
     require 'cart.php';
 ?>
+</body>
+</html>
+
 <?php require 'footer.php'; ?>
