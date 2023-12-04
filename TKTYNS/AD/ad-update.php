dd96077b0d46d0c19302699efa698350be94d084
SRtamas <2201222@s.asojuku.ac.jp>
2023-12-04 15:07:35 +0900

    ad-updateの完成

--- TKTYNS/AD/ad-update.php
+++ TKTYNS/AD/ad-update.php
@@ -34,54 +34,53 @@
 	
 		//var_dump($row);
 
-		        echo '<form action="ad-Update Completed.php" method="post">';
+		        echo '<form action="ad-Update Completed.php?id=', $_GET['id'],'" method="post">';
                 echo '<input type="hidden" name="shohin_number" value="',$row['shohin_number'],'required">';
-                //echo '<div class="td0">',$row['shohin_number'],'</div>';
                 echo '<tr>';
 
-	        	//echo '<div class="td1">';
+	        	echo '<div class="td1">';
                 echo '<td><input type="text" name="shohin_mei" value="',$row['shohin_mei'],'"required></td>';
 
-        		//echo '</div> ';
-		        //echo '<div class="td1">';
+        		echo '</div> ';
+		        echo '<div class="td1">';
                 echo '<td><input type="text" name="shohin_setu" value="',$row['shohin_setu'],'"required></td>';
-        		//echo '</div> ';
+        		echo '</div> ';
         
-                //echo '</div> ';
-                //echo '<div class="td1">';
+                echo '</div> ';
+                echo '<div class="td1">';
                 echo '<td><input type="text" name="shohin_price" value="',$row['shohin_price'],'"required></td>';
-                //echo '</div> ';
+                echo '</div> ';
 
-                //echo '</div> ';
-	        	//echo '<div class="td1">';
+                echo '</div> ';
+	        	echo '<div class="td1">';
                 echo '<td><input type="text" name="shohin_gazo" value="',$row['shohin_gazo'],'"required></td>';
-	        	//echo '</div> ';
+	        	echo '</div> ';
 
-                //echo '</div> ';
-		        //echo '<div class="td1">';
+                echo '</div> ';
+		        echo '<div class="td1">';
                 echo '<td><input type="text" name="shohin_sport" value="',$row2['shohin_sport'],'"required></td>';
-		        //echo '</div> ';
+		        echo '</div> ';
 
-                //echo '</div> ';
-		        //echo '<div class="td1">';
+                echo '</div> ';
+		        echo '<div class="td1">';
                 echo '<td><input type="text" name="shohin_burnd" value="',$row2['shohin_burnd'],'"rquired></td>';
-		        //echo '</div> ';
+		        echo '</div> ';
         
-                //echo '</div> ';
-	        	//echo '<div class="td1">';
+                echo '</div> ';
+	        	echo '<div class="td1">';
                 echo '<td><input type="text" name="shohin_kate" value="',$row2['shohin_kate'],'"rquired></td>';
-	        	//echo '</div> ';
+	        	echo '</div> ';
 
-                //echo '</div> ';
-		        //echo '<div class="td1">';
+                echo '</div> ';
+		        echo '<div class="td1">';
                 echo '<td><input type="text" name="konyu_kazu" value="',$row3['konyu_kazu'],'"rquired></td>';
-                //echo '</div> ';
+                echo '</div> ';
 
                 
-                //echo '</div> ';
-		        //echo '<div class="td1">';
+                echo '</div> ';
+		        echo '<div class="td1">';
                 echo '<td><input type="text" name="stock_kazu" value="',$row3['stock_kazu'],'"rquired></td>';
-                //echo '</div> ';
+                echo '</div> ';
 
                 echo '<input type="submit" value="更新"></div>';
 	        	echo '</form>';
