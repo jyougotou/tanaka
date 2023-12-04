c99523f9778ffb4abb9e72c63bdd2814ec2ebb41
SRtamas <2201222@s.asojuku.ac.jp>
2023-12-04 13:32:20 +0900

    ad-Upの途中

--- TKTYNS/AD/ad-Update Completed.php
+++ TKTYNS/AD/ad-Update Completed.php
@@ -15,15 +15,10 @@
 	<body>
 <?php
     $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('update product set name=?,price=? where id=?');
-    if (empty($_POST['name'])) {
-        echo '商品名を入力してください。';
-    } else
-    if (!preg_match('/[0-9]+/', $_POST['price'])) {
-        echo '商品価格を整数で入力してください。';
-    } else
-
-    if($sql->execute([htmlspecialchars($_POST['name']),$_POST['price'],$_POST['id']])){
+    $sql=$pdo->prepare('update Shohin set shoin_mei=?,shohin_setu=?,shohin_price=?,shohin_gazo=? where shohin_number=?');
+    $sql=$pdo->prepare('update Detail set shohin_sport=?,shohin_burnd=?,shohin_kate=? where shohin_number=?');
+    $sql=$pdo->prepare('update Stock set konyu_kazu=?,stock_kazu=? where shohin_number=?');
+    if($sql->execute([htmlspecialchars($_POST['shohin_mei']),($_POST['shohin_setu']),($_POST['shohin_price']),($_POST['shohin_gazo']),($_POST['shohin_sport']),($_POST['shohin_burnd']),($_POST['shohin_kate']),($_POST['konyu_kazu']),($_POST['shohin_number']),$_POST['shohin_number']])){
         // var_dump($sql);
         echo '更新に成功しました。';
     }else{
@@ -31,28 +26,6 @@
     }
     
 ?>
-        <hr>
-        <table>
-        <tr><th>商品名</th><th>商品説明</th><th>在庫</th><th>値段</th><th>ブランド名</th><th>カテゴリー</th><th>画像</th></tr>
-    </tr>
-    <?php
-     $sql=$pdo->prepare('select * from Shohin where shohin_number=?');
-     $sql->execute([$_GET['id']]);
-     foreach($sql as $row){
-        echo '<tr>';
-        echo '<td><input type = "text" name = "shohin_mei" value = "', $row['shohin_mei'], '"></td>';
-        echo '<td><input type = "text" name = "shohin_setu" value = "', $row['shohin_setu'], '"></td>';
-        echo '<td><input type = "text" name = "shohin_kazu" value = "', $row['shohin_kazu'], '"></td>';
-        echo '<td><input type = "text" name = "shohin_price" value = "', $row['shohin_price'], '"></td>';
-        echo '<td><input type = "text" name = "shohin_burnd" value = "', $row['shohin_burnd'], '"></td>';
-        echo '<td><input type = "text" name = "shohin_kate" value = "', $row['shohin_kate'], '"></td>';
-        echo '<td><input type = "text" name = "shohin_gazo" value = "', $row['shohin_gazo'], '"></td>';
-        echo '</tr>';
-     }
-    ?>
-
-
-        </table>
         <button onclick="location.href='ren6-6-input.php'">更新画面へ戻る</button>
     </body>
 </html>
