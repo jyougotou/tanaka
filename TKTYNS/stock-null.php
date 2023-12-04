378e5063ba9cc71a66d04158306d03ea1b8452aa
tazunokiyuto <148731141+tazunokiyuto@users.noreply.github.com>
2023-12-04 14:30:42 +0900

    hu

--- TKTYNS/stock-null.php
+++ TKTYNS/stock-null.php
@@ -1,8 +1,20 @@
 <?php require 'header.php'; ?>
 <?php require 'db-connect.php'; ?>
-    申し訳ございません。<br>
-    ただいま在庫がございません。
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/stock-null.css">
+    <title>Document</title>
+</head>
+<body>
+<p>申し訳ございません。</p>
+    <p>ただいま在庫がございません。</p>
     <form action = "product.php" method = "post">
-        <input type = "submit" value = "商品検索画面へ">
+        <input type = "submit" class = "button" value = "商品検索画面へ">
     </form>
+</body>
+</html>
+    
 <?php require 'footer.php'; ?>
