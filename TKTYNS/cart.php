43e3a5379496bf8b6d3bfaa2cebb413345900252
tazunokiyuto <148731141+tazunokiyuto@users.noreply.github.com>
2023-12-07 12:34:27 +0900

    2

--- TKTYNS/cart.php
+++ TKTYNS/cart.php
@@ -1,13 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/cart.css">
     <title>Cart</title>
 </head>
+<style>
+     .total {
+            background-color: #9d9b9b;
+            position: absolute;
+            position: fixed;
+            bottom: 25px;
+            border: 3px solid black;
+            margin-left: 20px;
+            padding: 15px 15px; /* サイズを大きく調整 */
+            color: white;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 18px;
+        }
+</style>
 <body>
-
+ 
 <?php
 if(!empty($_SESSION['Member'])){
     $pdo=new PDO($connect,USER,PASS);
@@ -19,7 +35,7 @@ if(!empty($_SESSION['Member'])){
         echo '<table>';
         echo '<tr class="header-row"><th class="product-name">商品名</th><th class="quantity">数量</th>';
         echo '<th class="plus-button">+</th><th class="minus-button">-</th><th class="product-description">商品説明</th><th class="delete-button">削除ボタン</th></tr>';
-        
+       
         $total = 0;
         //カートに商品の表示
         $sql=$pdo->prepare('select *
@@ -43,23 +59,23 @@ if(!empty($_SESSION['Member'])){
                 echo '<input type="hidden" name="shohin_number" value="', $row['shohin_number'], '">';
                 echo '<input type="submit" class="minus" value="-">';
                 echo '</form>';
-            echo '</td>'; 
+            echo '</td>';
             echo '<td>', $row['shohin_setu'] , '</td>';
             echo '<td>';
                 echo '<form action="cart-delete.php" method="post">';
                     echo '<input type="hidden" name="shohin_number" value="', $row['shohin_number'], '">';
                     echo '<input type="submit" class="productdelete" value="削除">';
                 echo '</form>';
-            echo '</td>'; 
+            echo '</td>';
             echo '</tr>';
         }
         echo '</table>';
-
+ 
 echo '<div class="total-section">';
 echo '届け先住所：',$_SESSION['Member']['member_stay'],'<br>';
-echo '購入者情報：',$_SESSION['Member']['member_mei'],'様 ',$_SESSION['Member']['member_fon'],' ',$total,'円';
+echo '購入者情報：',$_SESSION['Member']['member_mei'],'様 ',$_SESSION['Member']['member_fon'],' ','<p class="total">合計料金',$total,'円</p>';
 echo '</div>';
-
+ 
         echo '<form action = "purchase.php" method = "post">';
             echo '<input type = "submit" class="buy" value = "購入に進む">';
         echo '</form>';
@@ -71,11 +87,11 @@ echo '</div>';
     //ログインしていない場合
     echo '<div class="message">ログインしてください。</div>';
 }
-
+ 
 echo '<form action = "product.php" method = "post">';
     echo '<input type = "submit" class="back" value = "戻る">';
 echo '</form>';
-
+ 
 ?>
 </body>
 </html>
